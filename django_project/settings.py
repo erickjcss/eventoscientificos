23dc1f989a898dac80e6bc555777fa060a0f39ff
erickjcss <erickmiralles363@gmail.com>
2024-11-10 16:26:33 -0500

    fir

--- django_project/settings.py
+++ django_project/settings.py
@@ -161,14 +161,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+    STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Directorio donde Django buscará archivos estáticos
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Directorio donde Django buscará archivos estáticos específicos de la aplicación
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/pdfs', # Asegúrate de que 'pdfs' esté en la raíz de tu proyecto
+]
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
