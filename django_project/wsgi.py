bcecd723207bbf0bca1af60592231d2bef3ea5f6
erick2jcss <erickmiralles363@gmail.com>
2024-11-14 11:18:58 -0500

    first commit

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
