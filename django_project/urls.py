f2a29dfd59d4aebe8c83692105102aaa9e419440
erick2jcss <erickmiralles363@gmail.com>
2024-12-05 16:53:15 -0500

    first commit

--- django_project/urls.py
+++ django_project/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
      path('solicitud/',include('solicitud.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     
