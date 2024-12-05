ed7e3828c4194b8883a238835ec0d63d8ecd41c5
erick2jcss <erickmiralles363@gmail.com>
2024-12-05 18:08:52 -0500

    first commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 
 """ APP_NAME = os.environ.get("FLY_APP_NAME")"""
